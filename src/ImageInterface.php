e50b2181f6b409f6643295170955f37df2751d0c
VNClone <vietsmall@gmail.com>
2017-06-27 14:40:01 +0700

    Update ImageInterface.php

--- src/ImageInterface.php
+++ src/ImageInterface.php
@@ -1,11 +1,11 @@
 <?php
-namespace CSD\Image;
+namespace vnclone\Image;
 
-use CSD\Image\Metadata\Aggregate;
-use CSD\Image\Metadata\UnsupportedException;
-use CSD\Image\Metadata\Exif;
-use CSD\Image\Metadata\Iptc;
-use CSD\Image\Metadata\Xmp;
+use vnclone\Image\Metadata\Aggregate;
+use vnclone\Image\Metadata\UnsupportedException;
+use vnclone\Image\Metadata\Exif;
+use vnclone\Image\Metadata\Iptc;
+use vnclone\Image\Metadata\Xmp;
 
 /**
  * @author Daniel Chesterton <daniel@chestertondevelopment.com>
